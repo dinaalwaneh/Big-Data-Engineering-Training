eb3ebd975d02493e7fa74fbcb97802df03362d11
Dina Alwaneh <dbarahma8@gmail.com>
2022-06-13 11:17:00 +0300

    create Employee model class

--- Spring Boot/HR Project/hr-project/src/main/java/com/hr/model/Employee.java
+++ Spring Boot/HR Project/hr-project/src/main/java/com/hr/model/Employee.java
@@ -2,4 +2,56 @@ package com.hr.model;
 
 public class Employee {
 
+	//object i needed :
+	private long id;
+	private String firstName;
+	private String lastName;
+	private double salary;
+	private String address;
+	
+	
+	public Employee(long id, String firstName, String lastName, double salary, String address) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+		this.address = address;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+		
+	
 }

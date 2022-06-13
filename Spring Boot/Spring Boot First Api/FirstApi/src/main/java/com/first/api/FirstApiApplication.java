f68387ffe8a5cadb4996f0d8f02667d2fc60f01b
Dina Alwaneh <dbarahma8@gmail.com>
2022-06-13 09:38:17 +0300

    Initialize spring root project from sprintoolsuit4

---
+++ Spring Boot/Spring Boot First Api/FirstApi/src/main/java/com/first/api/FirstApiApplication.java
@@ -0,0 +1,13 @@
+package com.first.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApiApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/Spring Boot First Api/FirstApi/src/test/java/com/first/api/FirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

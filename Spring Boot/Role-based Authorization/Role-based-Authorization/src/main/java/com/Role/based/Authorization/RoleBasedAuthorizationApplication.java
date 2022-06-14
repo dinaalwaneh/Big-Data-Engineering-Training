0aa9b482feb4a80e84af6c5302738c3ccc2ea19b
Dina Alwaneh <dbarahma8@gmail.com>
2022-06-14 23:51:27 +0300

    initialize project

---
+++ Spring Boot/Role-based Authorization/Role-based-Authorization/src/main/java/com/Role/based/Authorization/RoleBasedAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.Role.based.Authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBasedAuthorizationApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/Role-based Authorization/Role-based-Authorization/src/test/java/com/Role/based/Authorization/RoleBasedAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Role.based.Authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

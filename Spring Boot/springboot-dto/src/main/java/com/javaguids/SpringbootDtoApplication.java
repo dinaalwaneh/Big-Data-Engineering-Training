b09cf41e3be469cc779a5ed59506632ee3de0517
Dina Alwaneh <dbarahma8@gmail.com>
2022-06-21 18:04:06 +0300

    demo project for spring boot and dto

---
+++ Spring Boot/springboot-dto/src/main/java/com/javaguids/SpringbootDtoApplication.java
@@ -0,0 +1,13 @@
+package com.javaguids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDtoApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/springboot-dto/src/test/java/com/javaguids/SpringbootDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

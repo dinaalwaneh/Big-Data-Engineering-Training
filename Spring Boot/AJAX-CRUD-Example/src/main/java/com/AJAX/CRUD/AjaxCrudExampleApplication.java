27b348ea0a3606fa44e2cd7d39c1a1f50728171c
Dina Alwaneh <dbarahma8@gmail.com>
2022-06-21 16:40:38 +0300

    set up inviroment for ajax crud example

---
+++ Spring Boot/AJAX-CRUD-Example/src/main/java/com/AJAX/CRUD/AjaxCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.AJAX.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxCrudExampleApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/AJAX-CRUD-Example/src/test/java/com/AJAX/CRUD/AjaxCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AJAX.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

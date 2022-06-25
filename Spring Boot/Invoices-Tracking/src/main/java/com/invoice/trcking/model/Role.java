db8301eda06244201727c19618cb00341f1ddd6a
Dina Alwaneh <dbarahma8@gmail.com>
2022-06-25 15:41:42 +0300

    update role

--- Spring Boot/Invoices-Tracking/src/main/java/com/invoice/trcking/model/Role.java
+++ Spring Boot/Invoices-Tracking/src/main/java/com/invoice/trcking/model/Role.java
@@ -2,35 +2,40 @@ package com.invoice.trcking.model;
 
 
 import javax.persistence.*;
-
-
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
+
 @Table
-@Entity
- 
+@Entity(name = "role")
 public class Role implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     public Long id;
     @Enumerated(EnumType.STRING )
-    @Column(unique = true,name="role_name")
+    @Column(name="role_Type", unique = true)
     private RoleType name;
   
+    //mappedBy = "role" -> role is the object in user entity :
     @OneToMany(mappedBy = "role", fetch = FetchType.LAZY,
             cascade = CascadeType.ALL)
     private Set<User> employees = new HashSet<>();
 
     
-    
+    //PS :  constructers , setters and getters i can replace them with annotations from lombok dependancy ->
+    //@Data -> for setters and getters 
+    //@ArgsConstructer & NoArgsConstructer 
+    //but it doesn't work with meee :)
+    //args constructer :
     public Role(Long id, RoleType name) {
 		super();
 		this.id = id;
 		this.name = name;
-	}
-
+	}	
+    
+    //setters and getters
 	public Long getId() {
 		return id;
 	}

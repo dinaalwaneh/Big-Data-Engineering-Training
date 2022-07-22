9cf593f0c18e5341676976f898f292d5d19bf4fc
Dina Alwaneh <dbarahma8@gmail.com>
2022-07-22 10:15:41 +0300

    create car table in hive + hdfs directory and import csv file to it

--- Apache Spark/Cars Theft ETL App/CarsTheftETLApp/src/main/resources/ddl/HiveQL.sql
+++ Apache Spark/Cars Theft ETL App/CarsTheftETLApp/src/main/resources/ddl/HiveQL.sql
@@ -1,6 +1,24 @@
 
 <!--create database -->
-CREATE DATABASE [IF NOT EXISTS] carssheft
+CREATE DATABASE [IF NOT EXISTS] car_sheft
 
 <!--use database-->
-use carssheft
+use car_sheft
+
+<!--create cars table-->
+CREATE TABLE IF NOT EXISTS car (
+ id long,
+ car_brand string,
+ Country_of_origin string)
+ COMMENT 'Car Table'
+ ROW FORMAT DELIMITED
+ FIELDS TERMINATED BY ',';
+
+<!--Create an HDFS directory-->
+hdfs dfs -mkdir warehouse
+
+<!-- Import cars.csv file into HDFS-->
+hdfs dfs -put [original-file-location] [hdfs-directory-name]
+
+<!--verify that the file is in the HDFS folder-->
+hdfs dfs -ls warehouse
